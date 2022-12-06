5a23ef35d3d4cc449531a8627c07ef412e8a49bf
alexsavinov <spell477@gmail.com>
2022-12-06 13:35:46 +0200

    lesson 04 - homework

---
+++ src/lesson04/hw04/Main.java
@@ -0,0 +1,23 @@
+package lesson04.hw04;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        ArrayList<Printable> printableList = new ArrayList<>();
+//
+//        printableList.add(new Book(1999, Publisher.BLOOMSBURY));
+//        printableList.add(new Book(2001, Publisher.LONGMAN));
+//        printableList.add(new Book(1956, Publisher.MACMILLAN));
+//        printableList.add(new Magazine(192, Language.UKR));
+//        printableList.add(new Magazine(60, Language.ENG));
+//
+//        System.out.println("----------------------------");
+//        System.out.println("List of objects with type Printable: " + printableList);
+//
+//        System.out.println("----------------------------");
+//        System.out.println("Calling method print() for each object:");
+//
+//        printableList.forEach(printable -> printable.print());
+    }
+}
